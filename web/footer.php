8b5a03f7cbf983c25ff2560c6d055d903e51bf4e
RichardsonRayshorn <rayshorndc@gmail.com>
2019-09-27 10:01:52 -0400

    personal homepage start modified js

---
+++ web/footer.php
@@ -0,0 +1,16 @@
+<?php 
+    echo '
+    <footer>
+    <div class="container">
+      <nav>
+        <ul>
+          <li>About</li>
+          <li>Assignments</li>
+        </ul>
+      </nav>
+    </div>
+  </footer>
+  <script src="./scripts/index.js"></script>
+</body>
+</html>
+    ';
--- web/index.php
+++ web/index.php
@@ -1,40 +1,18 @@
 <?php include_once 'header.php'; ?>
-<!-- <!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="./styles/index.css">
-    <title>Personal Homepage</title>
-  </head> -->
-  <body>
-    <header>
-      <nav id="navbar">
-        <img src="./images/close-512.png" alt="Close" id="close-menu">
-        <ul>
-          <li><a href="/">About</a></li>
-          <li><a href="">Assignments</a></li>
-        </ul>
-      </nav>
-    </header>
-    <div class="container">
-      <span class="hamburger-icon" id="hamburger"><img src="./images/hamburger_menu_9163.png" alt="Menu"></span>
-      <main>
-        <?php include "about.php"; ?>
-      </main>
-    </div>
-    <footer>
-      <div class="container">
-        <nav>
-          <ul>
-            <li>About</li>
-            <li>Assignments</li>
-          </ul>
-        </nav>
-      </div>
-    </footer>
-    <script src="./scripts/index.js"></script>
-  </body>
-  </html>
+<body>
+  <header>
+    <nav id="navbar">
+      <img src="./images/close-512.png" alt="Close" id="close-menu">
+      <ul>
+        <li><a href="/">About</a></li>
+        <li><a href="">Assignments</a></li>
+      </ul>
+    </nav>
+  </header>
+  <div class="container">
+    <span class="hamburger-icon" id="hamburger"><img src="./images/hamburger_menu_9163.png" alt="Menu"></span>
+    <main id="app">
+      <?php include "about.php"; ?>
+    </main>
+  </div>
+  <?php include_once 'footer.php'; ?>
