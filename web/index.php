5217bde2b110e680423b6abd9303ecbad1fdd20a
RichardsonRayshorn <rayshorndc@gmail.com>
2019-09-27 08:50:14 -0400

    personal homepage start modified js

--- web/index.php
+++ web/index.php
@@ -1,4 +1,4 @@
-<?php include 'header.php'; ?>
+<?php include_once 'header.php'; ?>
 <!-- <!DOCTYPE html>
   <html lang="en">
   <head>
@@ -14,30 +14,15 @@
       <nav id="navbar">
         <img src="./images/close-512.png" alt="Close" id="close-menu">
         <ul>
-          <li>About</li>
-          <li>Assignments</li>
+          <li><a href="/">About</a></li>
+          <li><a href="">Assignments</a></li>
         </ul>
       </nav>
     </header>
     <div class="container">
       <span class="hamburger-icon" id="hamburger"><img src="./images/hamburger_menu_9163.png" alt="Menu"></span>
       <main>
-        <div class="grid-col-2">
-          <div class="column first-col">
-            <p>Hello, I am <br/> <span class="author-name">Rayshorn Richardson</span></p>
-            <p>I'm a Software Engineer.</p>
-            <a href="" class="button">View My Work <span class="arrow"><img src="./images/arrow.png" alt="Assignments"></span></a>
-          </div>
-          <div class="column">
-            <span><img src="./images/profile.png" alt="profile pic" class="profile-image"></span>
-            <h2>My Journey</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
-              ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-              laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
-              voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
-              non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          </div>
-        </div>
+        <?php include "about.php"; ?>
       </main>
     </div>
     <footer>
