2cddcb22f8e8bd9481247959cd013cf0a92a210d
RichardsonRayshorn <rayshorndc@gmail.com>
2019-09-26 22:20:34 -0400

    personal homepage start

--- web/index.php
+++ web/index.php
@@ -6,16 +6,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="./styles/index.css">
-    <title>Team Assignment Week2</title>
+    <title>Personal Homepage</title>
   </head>
   <body>
-    <div>
+    <head>
+      <span><img src="./images/hamburger_menu_9163.png" alt="Menu"></span>
+      <nav>
+        <ul>
+          <li>About</li>
+          <li>Assignments</li>
+        </ul>
+      </nav>
+    </head>
+    <main>
       <div>
         <div>
-          <a href="#">Click Me</a>
+          <p>Hello, I'm Rayshorn Richardson</p>
+          <p>I'm a Software Engineer.</p>
+          <a href="">View My Work <span><img src="arrow.png" alt="Assignments"></span></a>
+        </div>
+        <div>
+           
         </div>
       </div>
-    </div>
+    </main>
+    <footer></footer>
     <script src="./scripts/index.js"></script>
   </body>
   </html>
