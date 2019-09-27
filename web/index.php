1e68b8de52b0e5c0e8d12a2296a3f3f5c2528a42
RichardsonRayshorn <rayshorndc@gmail.com>
2019-09-26 21:44:33 -0400

    personal homepage start

---
+++ web/index.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./styles/index.css">
+    <title>Team Assignment Week2</title>
+  </head>
+  <body>
+    <div>
+      <div>
+        <div>
+          <a href="#">Click Me</a>
+        </div>
+      </div>
+    </div>
+    <script src="./scripts/index.js"></script>
+  </body>
+  </html>
