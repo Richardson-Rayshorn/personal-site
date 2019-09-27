09e629ecba66fa70554cebc3334658d78412e8be
RichardsonRayshorn <rayshorndc@gmail.com>
2019-09-27 08:29:58 -0400

    personal homepage start modified js

--- web/scripts/index.js
+++ web/scripts/index.js
@@ -1,12 +1,12 @@
 //Get the hamburger to make it clickable
-document.getElementById("hamburger").addEventListener("click", visibleNav());
+document.getElementById("hamburger").addEventListener("click", visibleNav;
 
 function visibleNav(){
     //get the navbar and toggle it
     document.getElementById("navbar").classList.add("visible-nav");
 }
 
-document.getElementById("close-menu").addEventListener("click", invisibleNav());
+document.getElementById("close-menu").addEventListener("click", invisibleNav;
 
 function invisibleNav(){
     //get the navbar and toggle it
