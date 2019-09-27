9ae4cfbbc527c401aa3a0d96039aef3e8925b3ae
RichardsonRayshorn <rayshorndc@gmail.com>
2019-09-27 08:51:49 -0400

    personal homepage start modified js

---
+++ web/about.php
@@ -0,0 +1,22 @@
+<?php
+    echo '
+    <div class="grid-col-2">
+          <div class="column first-col">
+            <p>Hello, I am <br/> <span class="author-name">Rayshorn Richardson</span></p>
+            <p>I\'m a Software Engineer.</p>
+            <a href="" class="button">View My Work <span class="arrow"><img src="./images/arrow.png" alt="Assignments"></span></a>
+          </div>
+          <div class="column">
+            <span><img src="./images/profile.png" alt="profile pic" class="profile-image"></span>
+            <h2>My Journey</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+              ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+              laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+              voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+              non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+        </div>
+    
+    ';
+
+    ?>
